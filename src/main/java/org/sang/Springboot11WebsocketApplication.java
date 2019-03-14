843d9cfca2001b258f661973cdf6e7479f5374d9
Administrator <2495922311@qq.com>
2019-03-14 16:08:44 +0800

    springboot11_websocket

---
+++ src/main/java/org/sang/Springboot11WebsocketApplication.java
@@ -0,0 +1,13 @@
+package org.sang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11WebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot11WebsocketApplication.class, args);
+	}
+
+}
